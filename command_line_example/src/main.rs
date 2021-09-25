e2403169d3dae9af0d7c66f15789272d67ffafe1
Rahul Kaplesh <rahul.kaplesh@gmail.com>
2021-09-25 23:33:42 +0530

    Added function to search query

--- command_line_example/src/main.rs
+++ command_line_example/src/main.rs
@@ -1,21 +1,18 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use command_line_example::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_configs(&args);
-
-    println!("Query {} to be executed on {}", config.query, config.filename);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-fn parse_configs(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config{ query, filename };
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    
+    if let Err(e) = command_line_example::run(config) {
+        println!("Application error is : {}", e);
+        process::exit(1);
+    };
 }
